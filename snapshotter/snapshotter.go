6ad3c3d05bd1e088f546f7418fa86d20465a27d9
rectcircle <rectcircle96@gmail.com>
2023-10-05 00:14:09 +0800

    fix bug

--- snapshotter/snapshotter.go
+++ snapshotter/snapshotter.go
@@ -78,7 +78,7 @@ func (s *overlayCustomAddLowerSnapshotter) tryAddLowers(ctx context.Context, key
 	}
 	for i, o := range mounts[0].Options {
 		if strings.HasPrefix(o, "lowerdir=") {
-			mounts[0].Options[i] = lowerPathString + ":" + o
+			mounts[0].Options[i] = "lowerdir=" + lowerPathString + ":" + strings.TrimPrefix(o, "lowerdir=")
 			break
 		}
 	}
